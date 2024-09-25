0871c5a4750f606a9a3ee2e961b51bbb711e6f7f
bandisaiteja534 <bandisaiteja.57@gmail.com>
2024-09-26 04:19:05 +0530

    first commit

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,79 +1,90 @@
+
 const homelist = (req, res) => {
   res.render("locations-list", {
-    title: "Veltra- Travel Packages",
+    title: "Ojas Services- Wide Range space of Milk And Its Products",
     pageHeader: {
       title: "Veltra",
-      strapline: "Explore With us",
+      strapline: "Wide Range space of Milk And Its Products!",
     },
     sidebar:
-      "Discover the World, One Adventure at a Time!",
+      "Looking for Milk and a Sweet? Ojas helps you find places to when out and about. Perhaps with Milk, curd or a Sweet?Let Ojas help you find the place you're looking for.",
     locations: [
       {
-        name: "Veltra Services ",
-        address: "road no 6,Gawtham nagar,Hyderabad",
+        name: "Ojas Services ",
+        address: "Suryapet, Available, Road N0.6",
         rating: 4,
-        facilities: ["Travel Packages around the world"],
-        distance: "100m",
+        facilities: ["Milk", "Curd", "Sweets"],
+        distance: "1Km",
       },
       {
-        name: "Veltra Services",
-        address: "Road no 4,Pune,Maharastra",
+        name: "Ojas Milk",
+        address: "Rock Town Colony, Available, 1-45",
         rating: 4,
-        facilities: ["Travel packages",],
-        distance: "200m",
+        facilities: ["Fresh Milk", "Curd",],
+        distance: "20Km",
+      },
+      {
+        name: "Sweet Store",
+        address: "Siddipet, Available, Road No.2",
+        rating: 5,
+        facilities: ["Halwa", "kesari","Mysorepak","Cova"],
+        distance: "6Km",
       },
     ],
   });
 };
 const locationInfo = function (req, res) {
   res.render("location-info", {
-    title: "Veltra Services",
-    pageHeader: { title: "Veltra Services" },
+    title: "Ojas Services",
+    pageHeader: { title: "Ojas Services" },
     sidebar: {
       context:
-        "it is Veltra because we make people easy to explore all over the world.",
+        "is on Ojas because it has Milk and space to Eat the Sweets with your friends andnfamily and get some Joy.",
       callToAction:
         "If you've been and you like it - or if you don't -➥please leave a review to help other people just like you.",
     },
     location: {
-      name: "Hyderabad",
-      address: "Boduppal, Available, Road N0.6",
+      name: "Ojas Services",
+      address: "Suryapet, Available, Road N0.6",
       rating: 4,
-      facilities: ["Travel packages"],
+      facilities: ["Milk", "Curd", "Sweets"],
       coords: { lat: 51.455041, lng: -0.9690884 },
       openingTimes: [
         {
-          days: "Monday - Friday",
+          days: "Monday -Saturday",
           opening: "5:00am",
           closing: "9:00pm",
           closed: false,
         },
         {
-          days: "Saturday",
+          days: "Sunday",
           opening: "4:00am",
           closing: "9:00pm",
           closed: false,
         },
-        {
-          days: "Sunday",
-          closed: true,
-        },
+      
       ],
       reviews: [
-    
         {
-          author: "snigdha  ",
+          author: "Narendra  ",
+          rating: 5,
+          timestamp: "14 August 2024",
+          reviewText:
+            "Great place.",
+        },
+        {
+          author: "Udaya Sree  ",
           rating: 4,
           timestamp: "16 July 2024",
           reviewText:
             "What a great place. I can't say enough good things about it.",
         },
         {
-          author: "Sai Teja ",
+          author: "Sai Niheth ",
           rating: 3,
           timestamp: "21 June 2023",
           reviewText:
-            "It was okay. Halwa wasn't great, but the Milk and Curd werw Good.",
+            "It was okay. Halwa wasn't great, and the Milk and Curd were Good.",
         },
       ],
     },
@@ -81,12 +92,22 @@ const locationInfo = function (req, res) {
 };
 const addReview = function (req, res) {
   res.render("location-review-form", {
-    title: "Review Veltra on Veltra Services",
-    pageHeader: { title: "Review Veltra" },
+    title: "Review Ojas  on Ojas Services",
+    pageHeader: { title: "Review Ojas" },
   });
 };
+const contactPage = (req, res) => {
+  res.render("contact", {
+    title: "Contact Ojas Services",
+    pageHeader: { title: "Get in Touch with Ojas Services" },
+    sidebar: "Have questions or feedback? We'd love to hear from you!",
+  });
+};
+
+
 module.exports = {
   homelist,
   locationInfo,
   addReview,
+  contactPage,
 };
