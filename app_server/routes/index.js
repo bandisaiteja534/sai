d7469b2a1ac2274381a1f3fdf450dc3fea80e2d3
bandisaiteja534 <bandisaiteja.57@gmail.com>
2024-10-05 15:09:32 +0530

    first commit

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const ctrlLocations =
-require('../controllers/locations');
+const ctrlLocations = require('../controllers/locations');
 const ctrlOthers = require('../controllers/others');
+
 /* Locations pages */
 router.get('/', ctrlLocations.homelist);
-router.get('/location', ctrlLocations.locationInfo);
-router.get('/location/review/new', ctrlLocations.addReview);
+router.get('/Yogi', ctrlLocations.locationInfo); // Using a more RESTful route structure
+router.get('/Laxmi', ctrlLocations.locationInfo1);
+router.get('/The', ctrlLocations.locationInfo2);
+router.get('/locations/review/new', ctrlLocations.addReview);
+
 /* Other pages */
 router.get('/about', ctrlOthers.about);
 
