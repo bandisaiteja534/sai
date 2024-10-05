425d057a6ab408379bbb3ff2e6907d5750dc7da5
bandisaiteja534 <bandisaiteja.57@gmail.com>
2024-10-05 15:09:59 +0530

    first commit

--- app_server/routes/users.js
+++ app_server/routes/users.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+module.exports = router;var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
 module.exports = router;
